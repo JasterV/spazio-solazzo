2633c6b3d56acb4e4950be135b1fbae0253d3efa
JasterV <49537445+JasterV@users.noreply.github.com>
2025-12-16 13:52:58 +0100

    fix: E regex modifier is not supported yet on fly.io

--- config/dev.exs
+++ config/dev.exs
@@ -59,12 +59,12 @@ config :spazio_solazzo, SpazioSolazzoWeb.Endpoint,
     web_console_logger: true,
     patterns: [
       # Static assets, except user uploads
-      ~r"priv/static/(?!uploads/).*\.(js|css|png|jpeg|jpg|gif|svg)$"E,
+      ~r"priv/static/(?!uploads/).*\.(js|css|png|jpeg|jpg|gif|svg)$",
       # Gettext translations
-      ~r"priv/gettext/.*\.po$"E,
+      ~r"priv/gettext/.*\.po$",
       # Router, Controllers, LiveViews and LiveComponents
-      ~r"lib/spazio_solazzo_web/router\.ex$"E,
-      ~r"lib/spazio_solazzo_web/(controllers|live|components)/.*\.(ex|heex)$"E
+      ~r"lib/spazio_solazzo_web/router\.ex$",
+      ~r"lib/spazio_solazzo_web/(controllers|live|components)/.*\.(ex|heex)$"
     ]
   ]
 
